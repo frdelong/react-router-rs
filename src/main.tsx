a6f3604aff6bba570588d8d84b83026be71c1f4f
Fabio Delong <frdelong@gmail.com>
2025-09-08 03:24:03 -0300

    feat: replace App component with AppRoutes and add Home page

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import { BrowserRouter } from 'react-router'
 
 import './main.css'
 
-import App from './App.tsx'
+import { AppRoutes } from './routes/AppRoutes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>,
 )

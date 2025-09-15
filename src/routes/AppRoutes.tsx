c22ec7615a06d6c402f8c0478326f2a7d5d943fe
frdelong <frdelong@gmail.com>
2025-09-15 04:01:12 -0300

    feat: implement Layout component and update routing in AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,15 +4,19 @@ import { Home } from '../pages/Home'
 import { Products } from '../pages/Products'
 import { NotFound } from '../pages/NotFound'
 import { Details } from '../pages/Details'
+import { Layout } from '../components/layout'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' index element={<Home/>} />
-      <Route path='/products' element={<Products/> } />
-      <Route path='/details/:id' element={<Details />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" index element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Route>
 
-      <Route path='*' element={<NotFound />} />
+      <Route path="/details/:id" element={<Details />} />
+      
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
